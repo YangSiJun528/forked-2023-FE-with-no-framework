54e1fb7994d9f0d133c7f3cdb007b4e5dcbead76
sijun-yang <yangsijun5528@gmail.com>
2025-09-11 15:50:53 +0900

    Revert "Update registry.js"
    This reverts commit e95283d2ddba16011c057355adfdfe5fe66d1578.

--- 02. 렌더링/05. TodoMVC add dynamic data rendering/registry.js
+++ 02. 렌더링/05. TodoMVC add dynamic data rendering/registry.js
@@ -1,48 +1,44 @@
 const registry = {}
 
 const renderWrapper = component => {
-    return (targetElement, state) => {
-        // 현재 컴포넌트 실행
-        const element = component(targetElement, state)
+  return (targetElement, state) => {
+    const element = component(targetElement, state)
 
-        // 자식 컴포넌트 탐색
-        const childComponents = element.querySelectorAll('[data-component]')
+    const childComponents = element
+      .querySelectorAll('[data-component]')
 
-        Array
-            .from(childComponents)
-            .forEach(target => {
-                const name = target.dataset.component
-                // child의 타입도 renderWrapper()의 반환 결과인 (targetElement, state) => {}임.
-                const child = registry[name]
+    Array
+      .from(childComponents)
+      .forEach(target => {
+        const name = target
+          .dataset
+          .component
 
-                if (!child) {
-                    return // 등록되지 않은 컴포넌트는 무시
-                }
+        const child = registry[name]
+        if (!child) {
+          return
+        }
 
-                // 자식 DOM 요소를 해당 컴포넌트로 교체 (콜백 호출, 커링)
-                // (교체된 컴포넌트 내부에서도 같은 방식으로 재귀 탐색 수행)
-                target.replaceWith(child(target, state))
-            })
+        target.replaceWith(child(target, state))
+      })
 
-        return element
-    }
+    return element
+  }
 }
 
-// 컴포넌트를 받고 등록함.
 const add = (name, component) => {
-    registry[name] = renderWrapper(component)
+  registry[name] = renderWrapper(component)
 }
 
 const renderRoot = (root, state) => {
-    // cloneComponent = root를 복사한 가상 돔을 만들어 냄
-    const cloneComponent = root => {
-        return root.cloneNode(true)
-    }
+  const cloneComponent = root => {
+    return root.cloneNode(true)
+  }
 
-    return renderWrapper(cloneComponent)(root, state)
+  return renderWrapper(cloneComponent)(root, state)
 }
 
 export default {
-    add,
-    renderRoot
+  add,
+  renderRoot
 }
